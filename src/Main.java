a22f168332b0875f08fb49d2b567233743a2795b
Ilya <x2qop2@yandex.com>
2025-04-04 11:02:40 +0300

    Initial commit:task manager

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,18 @@ public class Main {
     public static void main(String[] args) {
         TaskManager manager = new TaskManager();
 
-        // Создаем задачи
-        Task task1 = new Task("Помыть посуду", "Помыть всю посуду вечером", Status.NEW);
+
+        Task task1 = new Task("11", "asdfg", Status.NEW);
         manager.createTask(task1);
 
-        // Создаем эпик с подзадачами
-        Epic epic1 = new Epic("Переезд", "Организация переезда в новый офис");
+
+        Epic epic1 = new Epic("222", "qwerty");
         manager.createEpic(epic1);
 
-        Subtask subtask1 = new Subtask("Собрать коробки", "Купить и собрать коробки", Status.NEW, epic1.getId());
+        Subtask subtask1 = new Subtask("33333", "zxcvbn", Status.NEW, epic1.getId());
         manager.createSubtask(subtask1);
 
-        // Выводим информацию
+
         System.out.println("Все задачи:");
         manager.getAllTasks().forEach(System.out::println);
 
@@ -23,7 +23,7 @@ public class Main {
         System.out.println("\nВсе подзадачи:");
         manager.getAllSubtasks().forEach(System.out::println);
 
-        // Меняем статусы
+
         subtask1.setStatus(Status.DONE);
         manager.updateSubtask(subtask1);
         System.out.println("\nПосле изменения статуса подзадачи:");
--- src/Task.java
+++ src/Task.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-
 public class Task {
     private int id;
     private String title;
@@ -12,36 +10,29 @@ public class Task {
         this.status = status;
     }
 
+    // Геттеры
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public Status getStatus() {
         return status;
     }
 
+    // Сеттеры (только необходимые)
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public void setStatus(Status status) {
         this.status = status;
     }
-
 }
